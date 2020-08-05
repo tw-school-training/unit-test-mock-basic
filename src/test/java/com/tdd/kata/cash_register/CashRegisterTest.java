17ce59b3a3feef6dbf7ce7a89d146f4241625017
Zhen She <neal.seah@gmail.com>
2020-08-05 15:50:57 +0800

    test: verify print with purchase using stub

--- src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
+++ src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
@@ -17,11 +17,40 @@ class CashRegisterTest {
 		assertTrue(spyPrinter.hasBeenCalledPrint);
 	}
 
+	@Test
+	void should_process_execute_printing_with_purchase() {
+		//given
+		SpyPrinter spyPrinter = new SpyPrinter();
+		CashRegister cashRegister = new CashRegister(spyPrinter);
+		final String ANY_CONTENT = "content2020-08-05T13:49:39.966861";
+		StubPurchase stubPurchase = new StubPurchase(ANY_CONTENT);
+		//when
+		cashRegister.process(stubPurchase);
+		//then
+		assertTrue(spyPrinter.hasBeenCalledPrint);
+		assertEquals(ANY_CONTENT, spyPrinter.content);
+	}
+
+	private class StubPurchase extends Purchase {
+		private String stubContent;
+
+		private StubPurchase(String stubContent) {
+			this.stubContent = stubContent;
+		}
+
+		@Override
+		public String asString() {
+			return stubContent;
+		}
+	}
+
 	private class SpyPrinter extends Printer {
 		public boolean hasBeenCalledPrint;
+		public String content;
 
 		@Override
 		public void print(String content) {
+			this.content = content;
 			hasBeenCalledPrint = true;
 			super.print(content);
 		}
