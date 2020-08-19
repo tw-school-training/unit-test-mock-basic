d27ce8c3f28bd65065b6da91860d75a90f5d4ae4
Zhaoyu <weixiaobo88@gmail.com>
2020-08-19 18:05:45 +0800

    feat: add spy printer to verify called

--- src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
+++ src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
@@ -2,16 +2,30 @@ package com.tdd.kata.cash_register;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class CashRegisterTest {
 	@Test
 	public void should_process_execute_printing() {
 		//given
-		CashRegister cashRegister = new CashRegister();
+		SpyPrinter spyPrinter = new SpyPrinter();
+		CashRegister cashRegister = new CashRegister(spyPrinter);
 		Purchase purchase = new Purchase();
 		//when
 		cashRegister.process(purchase);
 		//then
-		//verify that cashRegister.process will trigger print
+		assertTrue(spyPrinter.hasBeenCalledPrint);
 	}
 
+	private class SpyPrinter extends Printer {
+		boolean hasBeenCalledPrint;
+		String content;
+
+		@Override
+		public void print(String content) {
+			this.content = content;
+			hasBeenCalledPrint = true;
+			super.print(content);
+		}
+	}
 }
