23ca15359a71628f5207860348201a280ef18836
Zhaoyu <weixiaobo88@gmail.com>
2020-08-19 17:36:04 +0800

    use maven & gradle wrapper and use junit 4 instead of junit 5

--- src/main/java/com/tdd/kata/cash_register/CashRegister.java
+++ src/main/java/com/tdd/kata/cash_register/CashRegister.java
@@ -11,7 +11,7 @@ public class CashRegister {
 	}
 
 	void process(Purchase purchase) throws IllegalArgumentException {
-		if (purchase.asString() == null || purchase.asString().isBlank())
+		if (purchase.asString() == null || purchase.asString().isEmpty())
 			throw new IllegalArgumentException("Purchase is empty");
 		printer.print(purchase.asString());
 	}
--- src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
+++ src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
@@ -1,51 +1,48 @@
 package com.tdd.kata.cash_register;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
-class CashRegisterTest {
-	@Test
-	void should_process_execute_printing() {
-		//given
-		Printer spyPrinter = spy(new Printer());
-		CashRegister cashRegister = new CashRegister(spyPrinter);
-		Purchase purchase = new Purchase();
-		//when
-		cashRegister.process(purchase);
-		//then
-		verify(spyPrinter).print(any());
-	}
+public class CashRegisterTest {
+    @Test
+    public void should_process_execute_printing() {
+        //given
+        Printer spyPrinter = spy(new Printer());
+        CashRegister cashRegister = new CashRegister(spyPrinter);
+        Purchase purchase = new Purchase();
+        //when
+        cashRegister.process(purchase);
+        //then
+        verify(spyPrinter).print(any());
+    }
 
-	@Test
-	void should_process_execute_printing_with_purchase() {
-		//given
-		Printer spyPrinter = spy(new Printer());
-		CashRegister cashRegister = new CashRegister(spyPrinter);
-		final String ANY_CONTENT = "content2020-08-05T13:49:39.966861";
-		Purchase stubPurchase = mock(Purchase.class);
-		given(stubPurchase.asString()).willReturn(ANY_CONTENT);
-		//when
-		cashRegister.process(stubPurchase);
-		//then
-		verify(spyPrinter).print(ANY_CONTENT);
-	}
+    @Test
+    public void should_process_execute_printing_with_purchase() {
+        //given
+        Printer spyPrinter = spy(new Printer());
+        CashRegister cashRegister = new CashRegister(spyPrinter);
+        final String ANY_CONTENT = "content2020-08-05T13:49:39.966861";
+        Purchase stubPurchase = mock(Purchase.class);
+        given(stubPurchase.asString()).willReturn(ANY_CONTENT);
+        //when
+        cashRegister.process(stubPurchase);
+        //then
+        verify(spyPrinter).print(ANY_CONTENT);
+    }
 
-	@Test
-	void should_process_throw_exception_given_empty_purchase() {
-		//given
-		Printer printer = new Printer();
-		CashRegister cashRegister = new CashRegister();
-		Purchase stubPurchase = mock(Purchase.class);
-		given(stubPurchase.asString()).willReturn("");
-		//when
-		//then
-		assertThrows(IllegalArgumentException.class, () -> {
-			cashRegister.process(stubPurchase);
-		});
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void should_process_throw_exception_given_empty_purchase() {
+        //given
+        Printer printer = new Printer();
+        CashRegister cashRegister = new CashRegister();
+        Purchase stubPurchase = mock(Purchase.class);
+        given(stubPurchase.asString()).willReturn("");
+        //when
+        //then
+        cashRegister.process(stubPurchase);
+    }
 
 }
