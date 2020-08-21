560e74244b9b119f0d4e7e3c6f6a85528705f162
Zhaoyu <weixiaobo88@gmail.com>
2020-08-21 15:29:14 +0800

    use mock instead of spy

--- src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
+++ src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
@@ -10,34 +10,34 @@ public class CashRegisterTest {
     @Test
     public void should_trigger_print_when_process_given_printer_to_cash_register() {
         //given
-        Printer spyPrinter = spy(new Printer());
-        CashRegister cashRegister = new CashRegister(spyPrinter);
+        Printer mockPrinter = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(mockPrinter);
         Purchase purchase = new Purchase();
         //when
         cashRegister.process(purchase);
         //then
-        verify(spyPrinter).print(any());
+        verify(mockPrinter).print(any());
     }
 
     @Test
     public void should_trigger_print_with_purchase_content_when_process_given_printer_and_purchase_content() {
         //given
-        Printer spyPrinter = spy(new Printer());
-        CashRegister cashRegister = new CashRegister(spyPrinter);
+        Printer mockPrinter = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(mockPrinter);
         final String ANY_CONTENT = "content2020-08-05T13:49:39.966861";
         Purchase stubPurchase = mock(Purchase.class);
         given(stubPurchase.asString()).willReturn(ANY_CONTENT);
         //when
         cashRegister.process(stubPurchase);
         //then
-        verify(spyPrinter).print(ANY_CONTENT);
+        verify(mockPrinter).print(ANY_CONTENT);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void should_throw_exception_when_process_given_empty_purchase() {
         //given
         Printer printer = new Printer();
-        CashRegister cashRegister = new CashRegister();
+        CashRegister cashRegister = new CashRegister(printer);
         Purchase stubPurchase = mock(Purchase.class);
         given(stubPurchase.asString()).willReturn("");
         //when
