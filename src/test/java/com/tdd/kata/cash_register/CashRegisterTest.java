687c72420d821df1b36228c1b5156d4b34f3e7ba
Zhaoyu <weixiaobo88@gmail.com>
2020-08-21 15:19:57 +0800

    update test case name to match should_**_when_**_given_**

--- src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
+++ src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class CashRegisterTest {
 	@Test
-	public void should_process_execute_printing() {
+	public void should_trigger_print_when_process_given_printer_to_cash_register() {
 		//given
 		CashRegister cashRegister = new CashRegister();
 		Purchase purchase = new Purchase();
